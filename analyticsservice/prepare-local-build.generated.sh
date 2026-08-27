db9792b6f533b620e3dbfd01a6bf4313e068e8aa
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-27 23:42:20 +0300

    Generate Go module bindings inside Docker

--- analyticsservice/prepare-local-build.generated.sh
+++ analyticsservice/prepare-local-build.generated.sh
@@ -9,6 +9,14 @@ mkdir -p "$work_dir"
 find "$work_dir" -mindepth 1 -maxdepth 1 -exec rm -rf -- {} +
 cp -a "$source_dir/." "$work_dir/"
 
+for module in /modules/*; do
+  if [ -f "$module/Makefile" ]; then
+    make -C "$module" all TOOLS_DIR=/usr/local/bin \
+      PROTOC=/usr/local/bin/protoc OAPI_CODEGEN=/usr/local/bin/oapi-codegen
+  fi
+done
+
 cd "$work_dir"
 go mod edit -replace github.com/gorundebug/servicelib=/servicelib
 go mod edit -replace github.com/gorundebug/model=/modules/model
+make -f make.generated.mk gen-proto TOOLS_DIR=/usr/local/bin PROTOC=/usr/local/bin/protoc
--- automationservice/prepare-local-build.generated.sh
+++ automationservice/prepare-local-build.generated.sh
@@ -9,5 +9,13 @@ mkdir -p "$work_dir"
 find "$work_dir" -mindepth 1 -maxdepth 1 -exec rm -rf -- {} +
 cp -a "$source_dir/." "$work_dir/"
 
+for module in /modules/*; do
+  if [ -f "$module/Makefile" ]; then
+    make -C "$module" all TOOLS_DIR=/usr/local/bin \
+      PROTOC=/usr/local/bin/protoc OAPI_CODEGEN=/usr/local/bin/oapi-codegen
+  fi
+done
+
 cd "$work_dir"
 go mod edit -replace github.com/gorundebug/servicelib=/servicelib
+make -f make.generated.mk gen-proto TOOLS_DIR=/usr/local/bin PROTOC=/usr/local/bin/protoc
