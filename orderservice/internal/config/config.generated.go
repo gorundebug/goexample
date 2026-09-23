6f8bfa4bf973a5de3a2bb53f204862fad960a753
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-23 21:26:12 +0300

    Refresh canonical Go example

--- orderservice/internal/config/config.generated.go
+++ orderservice/internal/config/config.generated.go
@@ -714,23 +714,21 @@ func MakeConfig() *Config {
 				From: processOrderStreamID,
 				To:   splitPipelineStreamID,
 				CallSemantics: &cfg.CallSemanticsGroup{
-					TaskPool: &cfg.TaskPoolCallSemanticsConfig{
-						PoolName: "Default Pool",
-					},
+					FunctionCall: &cfg.FunctionCallSemanticsConfig{},
 				},
 			},
 			SplitPipelineToProcessOrderItems: cfg.LinkConfig{
 				From: splitPipelineStreamID,
 				To:   processOrderItemsStreamID,
 				CallSemantics: &cfg.CallSemanticsGroup{
-					ParallelCall: &cfg.ParallelCallSemanticsConfig{},
+					FunctionCall: &cfg.FunctionCallSemanticsConfig{},
 				},
 			},
 			SplitPipelineToSoftDeadline: cfg.LinkConfig{
 				From: splitPipelineStreamID,
 				To:   softDeadlineStreamID,
 				CallSemantics: &cfg.CallSemanticsGroup{
-					ParallelCall: &cfg.ParallelCallSemanticsConfig{},
+					FunctionCall: &cfg.FunctionCallSemanticsConfig{},
 				},
 			},
 		},
